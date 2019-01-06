d320c9c4a68224372c716c4c35f8dedcc6d5777a
xialiu1988 <liuxia198896@gmail.com>
2019-01-06 15:27:55 -0800

    'updated'

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is sunday!");
+        }
+    }
+}
